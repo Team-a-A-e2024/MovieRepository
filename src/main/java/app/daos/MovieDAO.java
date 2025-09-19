6b62226bf188dd5bd88017b500c9766e94a8dcdf
Iirne <emil@caerli.dk>
2025-09-20 00:40:53 +0200

    implemented test of getallbyperson and getallbygenre

--- src/main/java/app/daos/MovieDAO.java
+++ src/main/java/app/daos/MovieDAO.java
@@ -75,8 +75,6 @@ public class MovieDAO implements IDao<Movie, Integer> {
 
     public List<Movie> getAllByGenre(Genre genre) {
         try (EntityManager em = emf.createEntityManager()) {
-            //where g.id = :value
-
             Query query = em.createNativeQuery("SELECT * from movie m inner join public.movie_genre mg on m.id = mg.movies_id where mg.genres_id = :value" , Movie.class)
                     .setParameter("value", genre.getId());
 
@@ -85,7 +83,6 @@ public class MovieDAO implements IDao<Movie, Integer> {
         }
     }
 
-    //todo: testing
     public List<Movie> getAllByPerson(Person person) {
         try (EntityManager em = emf.createEntityManager()) {
             return em.createQuery("SELECT m FROM Person p join MovieCast c on c.person = p join Movie m on c.movie = m where p.id = :value", Movie.class)
